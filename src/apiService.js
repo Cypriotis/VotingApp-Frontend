3c71f673735d8e84f64523f851923ef832e790ca
Sotiris <it22131@hua.gr>
2024-06-07 21:49:43 +0300

    ssl

--- src/apiService.js
+++ src/apiService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://backend-574994684.eu-west-3.elb.amazonaws.com';
+const API_BASE_URL = 'http://backend-574994684.eu-west-3.elb.amazonaws.com:3000';
 
 
 const apiService = {
